ae62e9ea36efd062f177fb894e73bbfb553d9c8e
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-01-22 12:11:25 -0500

    0.0.2 Update
    -Added more Styling rules
    -Cleaned up index.html
    -Cleaned up footer
    -Added styling animations

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,11 @@ $(document).ready(function () {
   let contentOffset = getOffset();
   $(window).resize(() => (contentOffset = getOffset()));
 
-  /*
   $(window).scroll(() => {
     $(window).scrollTop() > contentOffset
-      ? $(".to-top").css("opacity", ".8")
+      ? $(".to-top").css("opacity", ".9")
       : $(".to-top").css("opacity", "0");
   });
-  */
 
   $(".to-top").click(() => {
     $("body, html").animate({ scrollTop: $("#navbar").offset().top }, 800);
