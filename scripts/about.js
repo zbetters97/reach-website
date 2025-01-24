0e8678409eaad87c46c6f1d6d0960e3d622944ef
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-01-24 14:39:41 -0500

    0.3.0 Update
    -Added About page with JS functionality
    -Cleaned up styling
    -Cleaned up coding

--- scripts/about.js
+++ scripts/about.js
@@ -1,3 +1,5 @@
+import { members } from "./data/members.js";
+
 $(document).ready(function () {
   const getOffset = () => $(".navbar").offset().top;
   let contentOffset = getOffset();
@@ -13,4 +15,69 @@ $(document).ready(function () {
     $("body, html").animate({ scrollTop: $("#navbar").offset().top }, 800);
     return !1;
   });
+
+  loadPage();
+  showMemberText();
 });
+
+function loadPage() {
+  renderMembersHTML();
+}
+
+function renderMembersHTML() {
+  let membersHTML = ``;
+
+  members.forEach((member) => {
+    const image = member.image;
+    const name = member.name;
+    const title = member.title;
+    const social = member.social;
+    const question = member.question;
+    const answer = member.answer;
+
+    membersHTML += `
+      <div class="about-member js-about-member">
+        <img class="about-member-img" src="${image}" />
+        <div class="about-member-info">
+          <div class="about-member-banner">
+            <h3 class="about-member-name">${name}</h3>
+            <div class="about-member-tag">
+              <h4 class="about-member-title">${title}</h4>
+              <a class="about-member-social" href="${social}" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div class="about-member-quote">
+            <p>
+              <strong>Q:</strong>
+              <em>${question}</em>
+            </p>
+            <p>
+              <strong>A:</strong>
+              "${answer}"
+            </p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    $("#js-members-container").html(membersHTML);
+  });
+}
+
+function showMemberText() {
+  $(".js-about-member").on("click", "img", function () {
+    const $target = $(this).siblings(".about-member-info");
+
+    if (!$target.hasClass("active")) {
+      $target.addClass("active");
+      $target.addClass("members-animate-on");
+      $target.removeClass("members-animate-off");
+    } else {
+      $target.removeClass("active");
+      $target.removeClass("members-animate-on");
+      $target.addClass("members-animate-off");
+    }
+  });
+}
---
+++ scripts/data/members.js
@@ -0,0 +1,48 @@
+export const members = [
+  {
+    name: "Tyler Schoenberger",
+    image: "images/about/tyler.png",
+    title: "Worship Leader",
+    social: "",
+    question: "What future goals do you guys have as a band?",
+    answer:
+      "Our goal is to continue to build our team with people who share a focus on Jesus Christ. It is incredible to hear our church sing our songs, which were all written to reflect God’s work at Reach Church. Continuing this pursuit is close to our hearts, and, as we move along, our hope is that we would continue to reach all people with hearing nothing but the name of Jesus through our music and our ministry.",
+  },
+  {
+    name: "Jeremy Betters",
+    image: "images/about/jeremy.png",
+    title: "Lead Guitar",
+    social: "",
+    question:
+      "What is the most important message you can give to new listeners?",
+    answer:
+      "At Reach Church, our mission has nothing to do with self gain. We believe that Jesus Christ is, has been, and always will be the center of everything that we as a band and a church stand for. My hope is that all our songs reflect the motto of nothing but Jesus and that this simple phrase will make a positive impact on all of our listener's lives.",
+  },
+  {
+    name: "Katie Betters",
+    image: "images/about/katie.png",
+    title: "Lead Vocalist",
+    social: "",
+    question: "Where do you pull your inspiration from as a singer?",
+    answer:
+      "Worshipping through songs of praise is what we were created to do. I find my inspiration to do this from the Bible. Colossians 3:16 says, 'Let the word of Christ dwell in you richly, teaching and admonishing one another in all wisdom, singing psalms and hymns and spiritual songs, with thankfulness in your hearts to God.",
+  },
+  {
+    name: "Jordan Schoenberger",
+    image: "images/about/jordan.png",
+    title: "Keys",
+    social: "",
+    question: "What is your favorite original song to perform?",
+    answer:
+      "My favorite song of ours to perform is ‘Nothing Else.’ I love this song and I love being able to sing this declaration with our church. We are about nothing but the name of Jesus, and we all exist to bring him glory. ‘Nothing Else’ reminds me that my greatest need and deepest desire is satisfied through Jesus Christ.",
+  },
+  {
+    name: "Marshall Smith",
+    image: "images/about/marshall.png",
+    title: "Drums",
+    social: "",
+    question: "How do you worship from the drum kit?",
+    answer:
+      "Engaging in worship for me can be challenging. Since I am sitting in an enclosed space that is far from the congregation, I often feel distant from everyone’s energy. But, I know that playing my instrument to the best of my God-given ability is a beautiful form of worship pleasing to Him. Realizing this allows me to be fully involved in the worship that we bring to God.",
+  },
+];
