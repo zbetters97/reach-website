394c2678a750acd0938f6a911bc07a5019728abd
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-02-03 11:46:41 -0500

    0.8.6 Update
    -Added JS form validation to login and signup

--- scripts/login.js
+++ scripts/login.js
@@ -2,4 +2,30 @@ import { handleWindow } from "./utils/window.js";
 
 $(document).ready(function () {
   handleWindow();
+  handleLogin();
 });
+
+function handleLogin() {
+  $("#js-login-form :input").each(function () {
+    $(this).on("click", function () {
+      $(this).removeClass("invalid-field");
+    });
+  });
+
+  $("#js-login-form").submit(function () {
+    checkEmptyForm($(this));
+
+    const username = $("#js-login-username");
+    const password = $("#js-login-password");
+
+    return !!username.val() && !!password.val();
+  });
+}
+
+function checkEmptyForm(form) {
+  form.find(":input:not(:button)").each(function () {
+    !$(this).val()
+      ? $(this).addClass("invalid-field")
+      : $(this).removeClass("invalid-field");
+  });
+}
--- scripts/signup.js
+++ scripts/signup.js
@@ -2,4 +2,47 @@ import { handleWindow } from "./utils/window.js";
 
 $(document).ready(function () {
   handleWindow();
+  handleSignup();
 });
+
+function handleSignup() {
+  $("#js-signup-form :input:not(:button)").each(function () {
+    $(this).on("click", function () {
+      $(this).removeClass("invalid-field");
+    });
+  });
+
+  $("#js-signup-form").submit(function () {
+    let isValid = true;
+
+    $(this)
+      .find(":input:not(:button)")
+      .each(function () {
+        checkEmptyInput($(this));
+
+        $(this).val() === "" && (isValid = false);
+      });
+
+    const email = $("#js-signup-email");
+    const password = $("#js-signup-password");
+    const repassword = $("#js-signup-repassword");
+
+    if (!~email.val().indexOf("@")) {
+      email.addClass("invalid-field");
+      isValid = false;
+    }
+    if (password.val() !== repassword.val()) {
+      password.addClass("invalid-field");
+      repassword.addClass("invalid-field");
+      isValid = false;
+    }
+
+    return isValid;
+  });
+}
+
+function checkEmptyInput(field) {
+  !field.val()
+    ? field.addClass("invalid-field")
+    : field.removeClass("invalid-field");
+}
